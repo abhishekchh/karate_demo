450d2b4dd16d3cb356564a7455bfceb4c0eba357
Abhishek Chhetri <abhishekchh@gmail.com>
2020-07-09 12:42:38 +0530

    update

--- src/test/java/com/tutorials/karate/TestRunner.java
+++ src/test/java/com/tutorials/karate/TestRunner.java
@@ -26,29 +26,30 @@ import org.junit.Test;
  *
  * @author pthomas3
  */
-@KarateOptions(tags = {"~@ignore"}) // important: do not use @RunWith(Karate.class) !
+@KarateOptions(tags = { "~@ignore" }) // important: do not use @RunWith(Karate.class) !
 public class TestRunner {
-    
-    @BeforeClass
-    public static void beforeClass() throws Exception {        
-      //  TestBase.beforeClass();
-    } 
-    
-    @Test
-    public void testParallel() {
-      //  System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
-        Results results = Runner.parallel(getClass(), 5);
-        generateReport(results.getReportDir());
-        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);        
-    }
-    
-    public static void generateReport(String karateOutputPath) {        
-        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
-        List<String> jsonPaths = new ArrayList(jsonFiles.size());
-        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "demo");
-        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
-        reportBuilder.generateReports();        
-    }
-    
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		// TestBase.beforeClass();
+	}
+
+	@Test
+	public void testParallel() {
+		// System.setProperty("karate.env", "demo"); // ensure reset if other tests
+		// (e.g. mock) had set env in CI
+		Results results = Runner.parallel(getClass(), 5);
+		generateReport(results.getReportDir());
+		assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+	}
+
+	public static void generateReport(String karateOutputPath) {
+		Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] { "json" }, true);
+		List<String> jsonPaths = new ArrayList(jsonFiles.size());
+		jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+		Configuration config = new Configuration(new File("target"), "demo");
+		ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+		reportBuilder.generateReports();
+	}
+
 }
